ec8d457650ccf8cb8cd38b91f6d46eacfe3c1434
SageAidoo <emmanuelaidoo02@gmail.com>
2023-07-14 21:10:45 -0700

    if variables and loops

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int number;
+
+    printf("Single-digit numbers of base 10: ");
+    for (number = 0; number <= 9; number++) {
+        printf("%d ", number);
+    }
+
+    printf("\n");
+
+    return 0;
+}
