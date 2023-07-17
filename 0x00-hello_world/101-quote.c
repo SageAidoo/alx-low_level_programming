5140772339a40305da269fa92a077ebd3df1ff7b
SageAidoo <emmanuelaidoo02@gmail.com>
2023-07-17 02:23:55 -0700

    C is fun

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,8 @@
+#!/bin/bash
+#include <stdio.h>
+int main (void)
+{
+	char Message []="and that piece of art is useful" - Dora Korpar, 2015-10-19"
+		write(Message);
+		return 0;
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,6 @@
+#/bin/bash
+#inclue <stdio.h>
+int main (void){
+	char Hello[] = "with proper grammar, but the outcome is a piece of art,";
+		printf(Hello);
+		return 0;}
