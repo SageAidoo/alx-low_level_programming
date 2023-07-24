f46ec2a4d7eb0f672c6934688e8c886d4db8746a
SageAidoo <emmanuelaidoo02@gmail.com>
2023-07-24 15:07:55 -0700

    reset updated

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,5 @@
 #include <stdio.h>
-void reset_to_98(int *n);
-int main() {
-int a = 10;
-printf("The value of a is: %d\n", a);
-reset_to_98(&a);
-printf("The value of a is now: %d\n", a);
-return 0;
-}
+int main ()
+void reset_to_98(int *n) {
+*n = 98;
+return 0;}
